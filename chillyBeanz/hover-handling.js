21e4865a498052cac3e7c7798f255eb658e44eb5
debabrata adhikari <dev23@pop-os.localdomain>
2023-08-17 18:29:59 +0530

    main-grid for tshirts some css changes

---
+++ chillyBeanz/hover-handling.js
@@ -0,0 +1,8 @@
+const gridElement = document.querySelectorAll('.apparel-card');
+
+for(i = 0; i<gridElement.length; i++){
+    gridElement[i].addEventListener('mouseover', function(e){
+        
+        
+    })
+}
