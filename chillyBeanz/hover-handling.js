7efa427f5ba3be56b88054180053757e41a91682
debabrata adhikari <dev23@pop-os.localdomain>
2023-08-23 19:48:41 +0530

    tshirt page

--- chillyBeanz/hover-handling.js
+++
@@ -1,8 +0,0 @@
-const gridElement = document.querySelectorAll('.apparel-card');
-
-for(i = 0; i<gridElement.length; i++){
-    gridElement[i].addEventListener('mouseover', function(e){
-        
-        
-    })
-}
